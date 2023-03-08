1c7d6609da709d29678bfd675bf43c3d755f4e1b
Hernán Romano <hernan.romanoc@gmail.com>
2023-03-08 18:28:18 +1100

    Streaming JSON for answer 1 with chunks

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 from utils.classes import reading_json, process_data
-from utils.variables import json_tweeter, json_geo, json_enrich
 import ijson
 import pandas as pd
 
 def main():
     #Read Json Files
     read = reading_json()
-    # chunk_size = 100 // Maybe it will be nice to process in Streamed Batches
+    chunk_size = 800
     summary_df1 = pd.DataFrame(columns = ['Greater Capital City', 'Number of Tweets Made'])
+    chunk_counter = 0
+    small_df_of_tweets_ready_to_be_processed = pd.DataFrame(columns = ['auth_id', 'place_name'])
     with open("./input/twitter-data-small.json", "rb") as f:
-
         for tweet in ijson.items(f, "item"):
-            # read = json.load(chunk)
             df_tw = read.read_tweets(tweet)
-            # df_geo = read.read_geo(json_geo, json_enrich)
-
-            #Process the data to get the
-            proc = process_data(df_tw)
+            small_df_of_tweets_ready_to_be_processed = pd.concat([small_df_of_tweets_ready_to_be_processed, df_tw])
+            chunk_counter = chunk_counter + 1
+            if chunk_counter == chunk_size:
+                #Process the data to get the
+                print(small_df_of_tweets_ready_to_be_processed)
+                proc = process_data(small_df_of_tweets_ready_to_be_processed)
+                chunk_counter = 0
+                small_df_of_tweets_ready_to_be_processed = pd.DataFrame(columns = ['auth_id', 'place_name'])
+        
+        if chunk_counter != 0:
+            print(f"SEND LAST CHUNK of size: {chunk_counter}")
 
             #Answer question 1
-            df1 = proc.point_1()
-            assert df_tw.shape[0] == df1.iloc[:,1].sum()
-            summary_df1 = pd.concat([summary_df1, df1]).groupby(['Greater Capital City']).sum().reset_index()
-
-            #Answer question 2
-            # df2 = proc.point_2()
-            # assert df_tw.shape[0] == df2.iloc[:,2].sum()
-            #print(df2.iloc[:10,:])
-
-            # #Answer question 3
-            # df3 = proc.point_3()
+            # df1 = proc.point_1()
+            # assert df_tw.shape[0] == df1.iloc[:,1].sum()
+            # summary_df1 = pd.concat([summary_df1, df1]).groupby(['Greater Capital City']).sum().reset_index()
 
     #Docs to csv
     output_folder_path = './output/'
     summary_df1.to_csv(output_folder_path+'df1.csv')
-    # df2.to_csv(output_folder_path+'df2.csv')
-    # df3.to_csv(output_folder_path+'df3.csv')
 
 if __name__ == '__main__':
     main()
--- utils/classes.py
+++ utils/classes.py
@@ -1,20 +1,17 @@
 #Import working libraries
 import pandas as pd
-import json
-from .variables import ccities, replacements, gcca_codes
+from .variables import replacements, gcca_codes
 
 class reading_json:
     def read_tweets(self, data):
         # Reading the twitter json
-        # with open("./input/twitter-data-small.json", "r") as file:
-        #     data = json.load(file)
 
         self.df = pd.DataFrame({'auth_id':[data['data']['author_id']],\
                 'place_name': [data['includes']['places'][0]['full_name']]})
     
         return self.df
     
-    def read_geo(self, json_geo, json_enrich):
+    def read_geo(self, json_geo):
         #Reading geographical data
         self.df_1 = pd.read_json(json_geo, orient='index')
         self.df_1  = self.df_1.reset_index(names='states')
@@ -35,54 +32,8 @@ class process_data:
         self.wk_ds['place_name'] = df[['place_name']].replace({'place_name':replacements}, regex=True)
 
     def point_1(self):
-        #Here we haven't used yet the sal file. Plus the non Australian territories must be grouped in other categorie
+        #Here we haven't used yet the sal file. Plus the non Australian territories must be grouped in other categories
         self.pnt_1 = self.wk_ds['place_name'].value_counts().reset_index()
         self.pnt_1['index'] = self.pnt_1['index']+" ("+self.pnt_1.replace({"index": gcca_codes}).iloc[:,0]+")"
         self.pnt_1.columns = ['Greater Capital City', 'Number of Tweets Made']
         return self.pnt_1
-    
-    def point_2(self):
-        #Top 10 tweeters with their number of tweets
-        self.pnt_2 = self.wk_ds['auth_id'].value_counts().reset_index().reset_index()
-        self.pnt_2.iloc[:,0] = (self.pnt_2.iloc[:,0] + 1).apply(lambda x: '#'+str(x))
-        self.pnt_2.columns = ['Rank', 'Author Id', 'Number of Tweets Made']
-        #pnt_2.iloc[:10,:].to_csv('pnt_2.csv')
-        return self.pnt_2
-    
-    def point_3(self):
-        #### Only Tweets from capital cities
-        # Group de data for the task
-        pnt_3 = self.wk_ds.reset_index()[self.wk_ds['place_name'].isin(ccities)]\
-                    [['auth_id','place_name','index']].groupby(['auth_id','place_name']).count().unstack().droplevel(0, axis=1)
-
-        # Create function to aggregate all in text
-        def series_agg(serie):
-            text = str(len(serie))+"(#"+str(serie.sum())+" tweets - "
-            for val in range(0,len(serie)):
-                text =text+str(serie.values[val])+"_"+serie.index[val]
-                if val < len(serie)-1:
-                    text += ", "
-                else:
-                    text += ")"
-            return text
-        
-        # Ordering by more locations and number of tweets
-        pnt_3_ = pnt_3.agg(func=['count','sum'], axis=1).reset_index().sort_values(['count','sum'], ascending=[False, False]).\
-                    reset_index(drop=True).reset_index(names='Rank').drop(['count','sum'],axis=1)
-
-        # Create ranking variable
-        pnt_3_['Rank'] = pnt_3_['Rank']+1
-
-        # Change columns names
-        pnt_3_.columns = ['Rank', 'Author Id']
-
-        # Agregate twits and locations in text cells
-        agreg = []
-        for user in pnt_3_['Author Id']:
-            agreg.append(series_agg(pnt_3.loc[user,:].dropna().astype(int)))
-        pnt_3_['Number of Unique City Locations and #Tweets'] = agreg
-        return pnt_3_
-
-    
-
-        
