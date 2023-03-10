f437af93bef9924d8b0dc3315f3831aa4cb66679
Andrés Gutiérrez <guticar94@gmail.com>
2023-03-10 21:49:44 +1100

    processing integration

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
-from utils.classes import reading_json, process_data
-import ijson
+from utils.variables import json_twitter, json_geo, gcca_codes, ccities
+from utils.variables import states_dict, capitals_dict, replacements
+from utils.classes import quality_data, reading_json, process_data
 import pandas as pd
 
 def main():
-    #Read Json Files
-    read = reading_json()
-    chunk_size = 800
-    summary_df1 = pd.DataFrame(columns = ['Greater Capital City', 'Number of Tweets Made'])
+    chunk_size = 100
     chunk_counter = 0
-    small_df_of_tweets_ready_to_be_processed = pd.DataFrame(columns = ['auth_id', 'place_name'])
-    with open("./input/twitter-data-small.json", "rb") as f:
-        for tweet in ijson.items(f, "item"):
-            df_tw = read.read_tweets(tweet)
-            small_df_of_tweets_ready_to_be_processed = pd.concat([small_df_of_tweets_ready_to_be_processed, df_tw])
-            chunk_counter = chunk_counter + 1
-            if chunk_counter == chunk_size:
-                #Process the data to get the
-                print(small_df_of_tweets_ready_to_be_processed)
-                proc = process_data(small_df_of_tweets_ready_to_be_processed)
-                chunk_counter = 0
-                small_df_of_tweets_ready_to_be_processed = pd.DataFrame(columns = ['auth_id', 'place_name'])
-        
-        if chunk_counter != 0:
-            print(f"SEND LAST CHUNK of size: {chunk_counter}")
 
-            #Answer question 1
-            # df1 = proc.point_1()
-            # assert df_tw.shape[0] == df1.iloc[:,1].sum()
-            # summary_df1 = pd.concat([summary_df1, df1]).groupby(['Greater Capital City']).sum().reset_index()
+    # Read Json Files
+    read = reading_json()
+    df_tw = read.read_tweets(json_twitter, chunk_size)          #Twitter file -- DataFrame
+    df_geo = read.read_geo(json_geo)                            #Sal file -- Dict
+
+    # Estandarize dataset 
+    qua = quality_data(df_tw, df_geo)
+    est = qua.standarize(states_dict)
+    est = qua.replacement(capitals_dict, replacements)
+
+
+    
+    # small_df_of_tweets_ready_to_be_processed = pd.DataFrame(columns = ['auth_id', 'place_name'])
+        #     small_df_of_tweets_ready_to_be_processed = pd.concat([small_df_of_tweets_ready_to_be_processed, df_tw])
+        #     chunk_counter = chunk_counter + 1
+        #     if chunk_counter == chunk_size:
+        #         #Process the data to get the
+        #         print(small_df_of_tweets_ready_to_be_processed)
+        #         proc = process_data(small_df_of_tweets_ready_to_be_processed)
+        #         chunk_counter = 0
+        #         small_df_of_tweets_ready_to_be_processed = pd.DataFrame(columns = ['auth_id', 'place_name'])
+        
+        # if chunk_counter != 0:
+        #     print(f"SEND LAST CHUNK of size: {chunk_counter}")
+
+    # Process data
+    proc = process_data(est)
+    #Answer question 1
+    df1 = proc.point_1(gcca_codes)
+    assert df_tw.shape[0] == df1.iloc[:,1].sum()
+
+    #Answer question 2
+    df2 = proc.point_2()
+    assert df_tw.shape[0] == df2.iloc[:,2].sum()
+
+    #Answer question 3
+    df3 = proc.point_3(ccities)
 
     #Docs to csv
     output_folder_path = './output/'
-    summary_df1.to_csv(output_folder_path+'df1.csv')
+    df1.to_csv(output_folder_path+'df1.csv')
+    df2.to_csv(output_folder_path+'df2.csv')
+    df3.to_csv(output_folder_path+'df3.csv')
 
 if __name__ == '__main__':
     main()
--- utils/classes.py
+++ utils/classes.py
@@ -1,39 +1,101 @@
 #Import working libraries
 import pandas as pd
-from .variables import replacements, gcca_codes
+import re
+import ijson
 
 class reading_json:
-    def read_tweets(self, data):
-        # Reading the twitter json
-
-        self.df = pd.DataFrame({'auth_id':[data['data']['author_id']],\
-                'place_name': [data['includes']['places'][0]['full_name']]})
-    
+    # Read Twitter dataframe function
+    def read_tweets(self, path, chunk_size):
+        # Define dict to append values
+        tweets = {'auth_id':[],'place_name':[]}
+        # Reading the twitter json with list Comprehension
+        with open(path, "rb") as f:
+                self.df = pd.DataFrame([{
+                    'auth_id':[tweet['data']['author_id']][0],\
+                    'place_name': [tweet['includes']['places'][0]['full_name']][0]}\
+                    for num, tweet in enumerate(ijson.items(f, "item")) if num < chunk_size])
         return self.df
     
-    def read_geo(self, json_geo):
-        #Reading geographical data
-        self.df_1 = pd.read_json(json_geo, orient='index')
-        self.df_1  = self.df_1.reset_index(names='states')
-        self.df_1  = self.df_1.set_index('sal').sort_index()
+    # Read SAL (Statistical Area Level) dataframe
+    def read_geo(self, path):
+        #Reading geographical data 
+        return pd.read_json(path, orient='index')['gcc']
 
-        #Enriching geographical data
-        self.df_2 = pd.read_csv('./input/georef.csv', sep=';').drop(['Geo Shape'], axis=1)
-        self.df_2 = self.df_2.set_index('Official Code Suburb').sort_index()
+class quality_data:
+    def __init__(self, df, geo):
+        # Read data
+        self.wk_ds = df
+        self.geo = geo
 
-        #Joinin geo data
-        self.df_geo = self.df_2.join(self.df_1).drop(['Iso 3166-3 Area Code', 'Year', 'states', 'ste', 'Geo Point'], axis = 1).reset_index(names = 'SA3')
-        return self.df_geo
-    
+    def standarize(self, states_dict):
+        # Standarize geo notation and change of structure to string comparison
+        self.geo.index = [val.lower().replace(' ', '').replace('.', '') for val in self.geo.index]
+        self.geo = self.geo.to_dict()
+
+        # Standarize states notation
+        for key, val in states_dict.items():
+            self.wk_ds['place_name'] = self.wk_ds['place_name'].apply(lambda x: re.sub(key, val, x))
+        self.wk_ds['place_name'] = self.wk_ds['place_name'].apply(lambda x:x.replace(',', '').replace(' ', '').lower())
+
+    def replacement(self, ccap_dict, replacements):
+        # Replace capital cities to gcc (Custom Regexp dictionary)
+        self.wk_ds[['place_name']] = self.wk_ds[['place_name']].replace({'place_name':ccap_dict}, regex=True)
+        # Replace territories to Sal gcc (Regexp merge)
+        self.wk_ds['place_name'] = self.wk_ds[['place_name']].replace({'place_name':self.geo})
+        # # Replace remaining territories to gcc (Custom Regexp dictionary)
+        self.wk_ds['place_name'] = self.wk_ds[['place_name']].replace({'place_name':replacements}, regex=True)
+        return self.wk_ds
 
 class process_data:
     def __init__(self, df):
+        # Read data
         self.wk_ds = df
-        self.wk_ds['place_name'] = df[['place_name']].replace({'place_name':replacements}, regex=True)
 
-    def point_1(self):
-        #Here we haven't used yet the sal file. Plus the non Australian territories must be grouped in other categories
+    def point_1(self, gcca_codes):
+        #Non Australian territories may be grouped in other categorie
         self.pnt_1 = self.wk_ds['place_name'].value_counts().reset_index()
         self.pnt_1['index'] = self.pnt_1['index']+" ("+self.pnt_1.replace({"index": gcca_codes}).iloc[:,0]+")"
         self.pnt_1.columns = ['Greater Capital City', 'Number of Tweets Made']
         return self.pnt_1
+    
+    def point_2(self):
+        #Top 10 tweeters with their number of tweets
+        self.pnt_2 = self.wk_ds['auth_id'].value_counts().reset_index().reset_index()
+        self.pnt_2.iloc[:,0] = (self.pnt_2.iloc[:,0] + 1).apply(lambda x: '#'+str(x))
+        self.pnt_2.columns = ['Rank', 'Author Id', 'Number of Tweets Made']
+        #pnt_2.iloc[:10,:].to_csv('pnt_2.csv')
+        return self.pnt_2
+
+    def point_3(self, ccities):
+        #### Only Tweets from capital cities
+        # Group de data for the task
+        pnt_3 = self.wk_ds.reset_index()[self.wk_ds['place_name'].isin(ccities)]\
+                    [['auth_id','place_name','index']].groupby(['auth_id','place_name']).count().unstack().droplevel(0, axis=1)
+
+        # Create function to aggregate all in text
+        def series_agg(serie):
+            text = str(len(serie))+"(#"+str(serie.sum())+" tweets - "
+            for val in range(0,len(serie)):
+                text =text+str(serie.values[val])+" from "+serie.index[val]
+                if val < len(serie)-1:
+                    text += ", "
+                else:
+                    text += ")"
+            return text
+        
+        # Ordering by more locations and number of tweets
+        pnt_3_ = pnt_3.agg(func=['count','sum'], axis=1).reset_index().sort_values(['count','sum'], ascending=[False, False]).\
+                    reset_index(drop=True).reset_index(names='Rank').drop(['count','sum'],axis=1)
+
+        # Create ranking variable
+        pnt_3_['Rank'] = pnt_3_['Rank']+1
+
+        # Change columns names
+        pnt_3_.columns = ['Rank', 'Author Id']
+
+        # Agregate twits and locations in text cells
+        agreg = []
+        for user in pnt_3_['Author Id']:
+            agreg.append(series_agg(pnt_3.loc[user,:].dropna().astype(int)))
+        pnt_3_['Number of Unique City Locations and #Tweets'] = agreg
+        return pnt_3_
