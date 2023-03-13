7fc900511f1ec4e7fc20a81e9276fc1915194808
Andrés Gutiérrez <guticar94@gmail.com>
2023-03-13 19:56:05 +1100

    Adding questions answers, modifying code to python language and commenting some sections

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,8 @@
+# Import working libraries
 import pandas as pd
-from utils.classes import quality_data, process_data
+import re
+
+#Import variables
 from utils.variables import (
     json_geo,
     gcca_codes,
@@ -9,84 +12,172 @@ from utils.variables import (
     replacements,
 )
 
+# Temporal printing parameters
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+total_number_of_available_nodes = comm.Get_size()
+#____________________________________________________________________________________________________
+# Read SAL (Statistical Area Level) dataframe
+class reading_json:
+    def read_geo(self, path):
+        # Reading geographical data
+        return pd.read_json(path, orient="index")["gcc"]
+#____________________________________________________________________________________________________
+# Apply standarization rules and merge data by place name
+class quality_data:
+    def __init__(self, df, geo):
+        # Read data
+        self.wk_ds = df
+        self.geo = geo
+    # Standarize geo notation and change of structure to string comparison
+    def standarize(self, states_dict):
+        # Set the names to lower case and drop special characters except Parenthesis to geo data
+        self.geo.index = [
+            val.lower().replace(" ", "").replace(".", "") for val in self.geo.index
+        ]
+        self.geo = self.geo.to_dict()
 
-def process_tweets(read, df_tw, rank):
-    df_geo = read.read_geo(json_geo)  # Sal file -- Dict
+        # Standarize tweets data states notation as sal notation
+        for key, val in states_dict.items():
+            self.wk_ds["place_name"] = self.wk_ds["place_name"].apply(
+                lambda x: re.sub(key, val, x)
+            )
 
+        # Set the names to lower case and drop special characters except Parenthesis to tweets data
+        self.wk_ds["place_name"] = self.wk_ds["place_name"].apply(
+            lambda x: x.replace(",", "").replace(" ", "").lower()
+        )
+
+    # Make data replacements to get standar structure of GCCA
+    def replacement(self, ccap_dict, replacements):
+        # Replace capital cities to gcc (Custom Regexp dictionary)
+        self.wk_ds[["place_name"]] = self.wk_ds[["place_name"]].replace(
+            {"place_name": ccap_dict}, regex=True
+        )
+        # Replace territories to Sal gcc (Regexp merge)
+        self.wk_ds["place_name"] = self.wk_ds[["place_name"]].replace(
+            {"place_name": self.geo}
+        )
+        # # Replace remaining territories to gcc (Custom Regexp dictionary)
+        self.wk_ds["place_name"] = self.wk_ds[["place_name"]].replace(
+            {"place_name": replacements}, regex=True
+        )
+        return self.wk_ds
+#____________________________________________________________________________________________________
+# Apply standarization and replace processing changes to incomming dataframes
+def process_tweets(df_tw, df_geo):
+
+    # Temporal printing to see results
+    print(f'Processing {df_tw.shape[0]} tweets on worker number {rank}')
+    
     # Estandarize dataset
     qua = quality_data(df_tw, df_geo)
     est = qua.standarize(states_dict)
     est = qua.replacement(capitals_dict, replacements)
 
-    # Process data
-    proc = process_data(est)
-    # Answer question 1
-    df1 = proc.point_1(gcca_codes)
-    assert df_tw.shape[0] == df1.iloc[:, 1].sum()
+    # Agreggated Data Frame and return estandarized agreggation
+    df = est.reset_index().groupby(['place_name', 'auth_id']).count().reset_index()
+    df.columns = ["Greater Capital City", "Author Id", "Number of Tweets Made"]
+    assert est.shape[0] == df.iloc[:, 2].sum()
+    return df
+#____________________________________________________________________________________________________
+# Responses to assignment questions
+class process_data:
+    def __init__(self, df):
+        # Read data
+        self.wk_ds = df
 
-    # Answer question 2
-    df2 = None
-    # df2 = proc.point_2()
-    # assert df_tw.shape[0] == df2.iloc[:, 2].sum()
+    def point_1(self, gcca_codes):
+        # In the end non Australian territories may be grouped in other categorie__________________________________
 
-    # Answer question 3
-    # df3 = proc.point_3(ccities)
-    return (df1, df2)
+        self.pnt_1 = self.wk_ds.groupby("Greater Capital City")['Number of Tweets Made'].sum().\
+                reset_index().sort_values('Number of Tweets Made', ascending = False)
+        self.pnt_1.iloc[:,0] = (
+            self.pnt_1.iloc[:,0] + " (" +
+            self.pnt_1.replace({'Greater Capital City': gcca_codes}).iloc[:, 0] +
+              ")")
+        return self.pnt_1
 
+    def point_2(self):
+        # Top 10 tweeters with their number of tweets
+        self.pnt_2 = self.wk_ds.groupby("Author Id")['Number of Tweets Made'].sum().reset_index().\
+                    sort_values('Number of Tweets Made', ascending = False)
+        self.pnt_2.iloc[:, 0] = self.pnt_2.iloc[:, 0].apply(lambda x: "#" + str(x))
+        return self.pnt_2
 
-def create_empty_data_frame_to_accumulate_chunks_of_tweets():
-    return pd.DataFrame(columns=["auth_id", "place_name"])
-
-
-def process_gathered_partial_results(partial_results, result_aggregator):
-    partial_results_for_q1, partial_results_for_q2 = zip(*partial_results)
-
-    # Aggregating partials results for q1
-    result_aggregator.update_results_question_1(partial_results_for_q1)
-
-
-def process_residual_tweets(
-    total_number_of_available_nodes,
-    collection_of_buckets,
-    tweet_counter,
-    bucket_of_individual_tweets,
-    comm,
-    read,
-    rank,
-    result_aggregator,
-):
-    # If chunk list is not completed, then create None Objects
-    if (
-        len(collection_of_buckets) < total_number_of_available_nodes
-        and len(collection_of_buckets) != 0
-    ):
-        collection_of_buckets.append(bucket_of_individual_tweets)
-        collection_of_buckets = collection_of_buckets + [None] * (
-            total_number_of_available_nodes - len(collection_of_buckets)
+    def point_3(self, ccities):
+        #### Only Tweets from capital cities
+        # Group de data for the task
+        pnt_3 = (
+            self.wk_ds.reset_index()[self.wk_ds["Greater Capital City"].
+                                     isin(ccities)][["Author Id", "Greater Capital City", "Number of Tweets Made"]]
+                                     .groupby(["Author Id", "Greater Capital City"])
+                                     ['Number of Tweets Made'].sum()
+                                     .unstack()
+                                    #  .droplevel(0, axis=1)
         )
-        send_buckets_to_workers(
-            collection_of_buckets, comm, rank, read, result_aggregator
+
+        # Create function to aggregate all in text
+        def series_agg(serie):
+            text = str(len(serie)) + "(#" + str(serie.sum()) + " tweets - "
+            for val in range(0, len(serie)):
+                text = text + str(serie.values[val]) + " from " + serie.index[val]
+                if val < len(serie) - 1:
+                    text += ", "
+                else:
+                    text += ")"
+            return text
+
+        # Ordering by more locations and number of tweets
+        pnt_3_ = (
+            pnt_3.agg(func=["count", "sum"], axis=1)
+            .reset_index()
+            .sort_values(["count", "sum"], ascending=[False, False])
+            .reset_index(drop=True)
+            .reset_index(names="Rank")
+            .drop(["count", "sum"], axis=1)
         )
-        update_signal_for_workers(False, comm)
-    elif tweet_counter != 0:
-        data_processed = process_tweets(read, bucket_of_individual_tweets, rank)
-        process_gathered_partial_results([data_processed], result_aggregator)
-        # Here we process the residual tweets (on rank 0 process) and tell the other workers to STOP
-        collection_of_buckets = [None] * total_number_of_available_nodes
-        comm.scatter(collection_of_buckets, root=0)
-        update_signal_for_workers(False, comm)
 
+        # Create ranking variable
+        pnt_3_["Rank"] = pnt_3_["Rank"] + 1
 
-def send_buckets_to_workers(
-    bucket_of_tweets_chunks, comm, rank, read, result_aggregator
-):
-    data = comm.scatter(bucket_of_tweets_chunks, root=0)
+        # Change columns names
+        pnt_3_.columns = ["Rank", "Author Id"]
 
-    data_procesed = process_tweets(read, data, rank)
-    partial_results = comm.gather(data_procesed, root=0)
+        # Agregate twits and locations in text cells
+        agreg = []
+        for user in pnt_3_["Author Id"]:
+            agreg.append(series_agg(pnt_3.loc[user, :].dropna().astype(int)))
+        pnt_3_["Number of Unique City Locations and #Tweets"] = agreg
 
-    process_gathered_partial_results(partial_results, result_aggregator)
+        return pnt_3_
+#____________________________________________________________________________________________________
 
+class ResultAggregator:
+    def __init__(self):
+        self.df1 = pd.DataFrame(
+            columns=["Greater Capital City", "Author Id", "Number of Tweets Made"]
+        )
 
+    def update_aggregation(self, partial_results):
+        # print(partial_results)
+        for partial_result in partial_results:
+            if isinstance(partial_result, pd.DataFrame):
+                self.df1 = (
+                    pd.concat([self.df1, partial_result])
+                    .groupby(["Greater Capital City", "Author Id"])["Number of Tweets Made"]
+                    .sum()
+                    .reset_index()
+                )
+#____________________________________________________________________________________________________
+def send_buckets_and_gather_results(
+    collection_of_buckets, comm, df_geo, result_aggregator):
+    # This function scatter the processes to the worker nodes and gather its results
+    data = comm.scatter(collection_of_buckets, root=0) # Sending bucket-chunks to each node in the network (Here we can test only send to workers)
+    partial_results = comm.gather(process_tweets(data, df_geo), root=0) # Gathering results of processing nodes
+    result_aggregator.update_aggregation(partial_results) # Aggregating partials results
+
+#____________________________________________________________________________________________________
 def update_signal_for_workers(are_there_tweets_being_processed, comm):
     comm.bcast(are_there_tweets_being_processed, root=0)
